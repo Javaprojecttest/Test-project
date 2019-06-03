1e4c60bf524d5fd92c0c22b78dc9afa64fc8cfde
Javaprojecttest <51003655+Javaprojecttest@users.noreply.github.com>
2019-06-02 22:39:54 -0400

    Switchcase program

---
+++ TestProject-GIT/src/switchcase.java
@@ -0,0 +1,27 @@
+
+public class switchcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age = 16;
+
+switch(age){
+
+case 13:
+	System.out.println ("teenager");
+	break;
+case 18:
+	System.out.println("major");
+	break;
+case 20:
+	System.out.println ("Youngster");
+	break;
+	
+default:
+	System.out.println ("Adult");
+	
+}
+
+	}
+
+}

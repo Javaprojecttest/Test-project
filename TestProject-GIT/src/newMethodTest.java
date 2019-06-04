76deb5d92f2077eb9bb3cd8206927b0fc67acaec
Javaprojecttest <51003655+Javaprojecttest@users.noreply.github.com>
2019-06-04 13:34:44 -0400

    multiple class and methods

---
+++ TestProject-GIT/src/newMethodTest.java
@@ -0,0 +1,14 @@
+
+public class newMethodTest {
+public void newTextTest() {
+System.out.println("welcome to methods");
+}
+
+public void newTEXTTest2(){
+	System.out.println("welocme to  multiple methods");
+}
+
+ public void newTEXTTest3(){
+	System.out.println("Welcome to methods");
+}	
+}
--- TestProject-GIT/src/test2.java
+++ TestProject-GIT/src/test2.java
@@ -4,6 +4,12 @@ public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	newMethodTest newobject = new newMethodTest();
+	
+	newobject.newTextTest ();
+	newobject.newTEXTTest2();
+	newobject.newTEXTTest3();
+
 	}
 
 }
